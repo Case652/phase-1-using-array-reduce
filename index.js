55e7a54a73748d5a54f84c486a8bd046b871ffc0
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-09-14 08:54:14 -0500

    Complete

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//const initialValue = 0
+//const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue,initialValue);
+
+//total batteries = batch barrerys  .reduce().= 31
+//                                      ^MDN array----^^^
+//const totalBatteries = batteryBatches.reduce(function (accumulator, element) {
+//    return element + accumulator;
+//})                                                       ^Flat
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue);
